35b10c5b4eb309bc21762211732f48cb737ea8af
yujie <uxwzr@student.kit.edu>
2021-07-27 22:16:36 +0200

    Datenbank Aenderungen(try-catch Exception)

--- Server/src/main/java/model/database/Database.java
+++ Server/src/main/java/model/database/Database.java
@@ -10,16 +10,17 @@ import model.user.Avatar;
 import model.user.User;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Map;
 import java.util.UUID;
 
 public class Database implements IUserAccountManagerDatabase, IUserDatabase, IGlobalContextDatabase {
     private static final String dbURL = "jdbc:derby:ChatiDB;create=true";
     private static Database database;
-    //private Connection connection;
+    private Connection connection;
 
 /*
-    @Override
     public static Connection getConnection() {
 
         try {
@@ -37,27 +38,32 @@ public class Database implements IUserAccountManagerDatabase, IUserDatabase, IGl
  */
 
     @Override
-    public void addWorld(SpatialContext world) {
-        // TODO
-        /*
-        PreparedStatement ps = this.connection.prepareStatement("INSERT INTO WORLD(NAME, ID) values(?,?)");
-        ps.setString(1, world.getContextName());
-        ps.setString(2, world.getContextID());
-        ps.executeUpdate();
-        this.connection.close();
-         */
+    public void addWorld(SpatialContext world){
+        try {
+            PreparedStatement ps = this.connection.prepareStatement("INSERT INTO WORLD(NAME, ID) values(?,?)");
+            ps.setString(1, world.getContextName());
+            ps.setString(2, world.getContextID().getID());
+            ps.executeUpdate();
+            this.connection.close();
+        } catch (SQLException e) {
+            //System.out.print(e);
+        }
 
     }
 
     @Override
     public void removeWorld(SpatialContext world) {
-        // TODO
-        /*
-        String worldId = world.getContextID();
-        PreparedStatement ps = this.connection.prepareStatement("DELETE FROM WORLD WHERE ID = " + worldId);
-        ps.executeUpdate();
-        this.connection.close();
-         */
+
+        try {
+            String worldId = world.getContextID().getID();
+            PreparedStatement ps = this.connection.prepareStatement("DELETE FROM WORLD WHERE ID = " + worldId);
+            ps.executeUpdate();
+            this.connection.close();
+        } catch (SQLException e) {
+            //System.out.print(e);
+        }
+
+
     }
 
     @Override
@@ -81,7 +87,6 @@ public class Database implements IUserAccountManagerDatabase, IUserDatabase, IGl
     public boolean checkPassword(String username, String password) {
         return false; // TODO
     }
-
     @Override
     public void setPassword(User user, String newPassword) {
         // TODO
@@ -89,34 +94,46 @@ public class Database implements IUserAccountManagerDatabase, IUserDatabase, IGl
 
     @Override
     public void changeAvatar(User user, Avatar avatar) {
-        // TODO
-        /*
-        PreparedStatement ps = this.connection.prepareStatement("UPDATE USER SET AVATAR_NAME = " + avatar.getName +
-                "WHERE USER_ID = " + user.getUserID());
-        ps.executeUpdate();
-        this.connection.close();
-         */
+
+        try {
+            PreparedStatement ps = this.connection.prepareStatement("UPDATE USER SET AVATAR_NAME = " + avatar.getName() +
+                    "WHERE USER_ID = " + user.getUserID().toString());
+            ps.executeUpdate();
+            this.connection.close();
+        } catch (SQLException e) {
+            //System.out.print(e)
+        }
+
 
     }
 
     @Override
     public void updateLastOnlineTime(User user) {
-        // TODO
-        /*
-        PreparedStatement ps = this.connection.prepareStatement("UPDATE USER SET LAST_ONLINE_TIME = ???
-                WHERE USER_ID = " + user.getUserID());
-        ps.executeUpdate();
-        this.connection.close();
-         */
+
+        try {
+            PreparedStatement ps = this.connection.prepareStatement("UPDATE USER SET LAST_ONLINE_TIME = " + "???" +
+                    "WHERE USER_ID = " + user.getUserID().toString());
+            ps.executeUpdate();
+            this.connection.close();
+        } catch (SQLException e) {
+            //System.out.print(e);
+        }
+
+
     }
 
     @Override
     public void deleteAccount(User user) {
-         /*
-        PreparedStatement ps = this.connection.prepareStatement("DELETE FROM USER WHERE USER_ID = " + user.getUserID());
-        ps.executeUpdate();
-        this.connection.close();
-         */
+
+        try {
+            PreparedStatement ps = this.connection.prepareStatement("DELETE FROM USER WHERE USER_ID = "
+                        + user.getUserID().toString());
+            ps.executeUpdate();
+            this.connection.close();
+        } catch (SQLException e) {
+            //System.out.print(e);
+        }
+
 
     }
 
@@ -137,79 +154,109 @@ public class Database implements IUserAccountManagerDatabase, IUserDatabase, IGl
 
     @Override
     public void addFriendship(User first, User second) {
-        // TODO
-        /*
-        PreparedStatement ps = this.connection.prepareStatement("INSERT INTO FRIENDSHIP(USER_ID1, USER_ID2) values(?,?)");
-        ps.setString(1, first.getUserID());
-        ps.setString(2, second.getUserID());
-        ps.executeUpdate();
-        this.connection.close();
-         */
+
+        try {
+            PreparedStatement ps = this.connection.prepareStatement("INSERT INTO FRIENDSHIP(USER_ID1, USER_ID2) values(?,?)");
+            ps.setString(1, first.getUserID().toString());
+            ps.setString(2, second.getUserID().toString());
+            ps.executeUpdate();
+            this.connection.close();
+        } catch (SQLException e) {
+            //System.out.print(e);
+        }
+
     }
 
     @Override
     public void removeFriendship(User first, User second) {
-        // TODO
-        /*
-        PreparedStatement ps = this.connection.prepareStatement("DELETE FROM FRIENDSHIP WHERE (USER_ID1 = "
-                + first.getUserID() + " AND USER_ID2 = " + second.getUserID() + ") OR (USER_ID1 = "
-                + second.getUserID() + " AND USER_ID2 = " + first.getUserID() + ")");
-        ps.executeUpdate();
-        this.connection.close();
-         */
+
+        try {
+            PreparedStatement ps = this.connection.prepareStatement("DELETE FROM FRIENDSHIP WHERE (USER_ID1 = "
+                    + first.getUserID().toString() + " AND USER_ID2 = " + second.getUserID().toString() + ") OR (USER_ID1 = "
+                    + second.getUserID().toString() + " AND USER_ID2 = " + first.getUserID().toString() + ")");
+            ps.executeUpdate();
+            this.connection.close();
+        } catch (SQLException e) {
+            //System.out.print(e);
+        }
+
+
     }
 
     @Override
     public void addIgnoredUser(User ignoringUser, User ignoredUser) {
-        // TODO
+
         /*
-        PreparedStatement ps = this.connection.prepareStatement("INSERT INTO IGNORE(USER_ID, IGNORED_ID) values(?,?)");
-        ps.setString(1, ignoringUser.getUserID());
-        ps.setString(2, ignoredUser.getUserID());
-        ps.executeUpdate();
-        this.connection.close();
+        try {
+            PreparedStatement ps = this.connection.prepareStatement("INSERT INTO IGNORE(USER_ID, IGNORED_ID) values(?,?)");
+            ps.setString(1, ignoringUser.getUserID().toString());
+            ps.setString(2, ignoredUser.getUserID().toString());
+            ps.executeUpdate();
+            this.connection.close();
+        } catch (SQLException e) {
+            //System.out.print(e);
+        }
+
          */
+
+
     }
 
     @Override
     public void removeIgnoredUser(User ignoringUser, User ignoredUser) {
-        // TODO
+
         /*
-        PreparedStatement ps = this.connection.prepareStatement("DELETE FROM IGNORE WHERE USER_ID = "
-                + ignoringUser.getUserID() + " AND IGNORED_ID = " + ignoredUser.getUserID();
-        ps.executeUpdate();
-        this.connection.close();
+        try {
+            PreparedStatement ps = this.connection.prepareStatement("DELETE FROM IGNORE WHERE USER_ID = "
+                    + ignoringUser.getUserID() + " AND IGNORED_ID = " + ignoredUser.getUserID());
+            ps.executeUpdate();
+            this.connection.close();
+        } catch (SQLException e) {
+            //System.out.print(e);
+        }
+
          */
+
+
     }
 
     @Override
     public void addRole(User user, Context context, Role role) {
-        // TODO
-        /*
-        PreparedStatement ps = this.connection.prepareStatement("INSERT INTO ROLE_WITH_CONTEXT
-                (USER_ID, ROLE, CONTEXT_ID) values(?,?,?)");
-        ps.setString(1, user.getUserID());
-        ps.setString(2, role.name());
-        ps.serString(3, context.getContextID());
-        ps.executeUpdate();
-        this.connection.close();
-         */
+/*
+        try {
+            PreparedStatement ps = this.connection.prepareStatement("INSERT INTO ROLE_WITH_CONTEXT
+            (USER_ID, ROLE, CONTEXT_ID) values(?,?,?)");
+            ps.setString(1, user.getUserID());
+            ps.setString(2, role.name());
+            ps.serString(3, context.getContextID());
+            ps.executeUpdate();
+            this.connection.close();
+        } catch (SQLException e) {
+            //System.out.print(e);
+        }
+
+*/
     }
 
     @Override
     public void removeRole(User user, Context context, Role role) {
-        // TODO
-        /*
-        PreparedStatement ps = this.connection.prepareStatement("DELETE FROM ROLE_WITH_CONTEXT WHERE USER_ID = "
-                + user.getUserID() + " AND ROLE = " + role.name() + " AND CONTEXT_ID = " + context.getContextID();
-        ps.executeUpdate();
-        this.connection.close();
-         */
+/*
+        try {
+            PreparedStatement ps = this.connection.prepareStatement("DELETE FROM ROLE_WITH_CONTEXT WHERE USER_ID = "
+                    + user.getUserID() + " AND ROLE = " + role.name() + " AND CONTEXT_ID = " + context.getContextID();
+            ps.executeUpdate();
+            this.connection.close();
+        } catch (SQLException e) {
+            //System.out.print(e);
+        }
+*/
+
     }
 
     @Override
     public void addNotification(User user, Notification notification) {
         // TODO
+
     }
 
     @Override
@@ -219,26 +266,34 @@ public class Database implements IUserAccountManagerDatabase, IUserDatabase, IGl
 
     @Override
     public void addBannedUser(User user, SpatialContext world) {
-        // TODO
-        /*
-        PreparedStatement ps = this.connection.prepareStatement("INSERT INTO BAN
-                (USER_ID, WORLD_ID) values(?,?)");
-        ps.setString(1, user.getUserID());
-        ps.setString(2, world.getContextID());
-        ps.executeUpdate();
-        this.connection.close();
-         */
+/*
+        try {
+            PreparedStatement ps = this.connection.prepareStatement("INSERT INTO BAN
+            (USER_ID, WORLD_ID) values(?,?)");
+            ps.setString(1, user.getUserID());
+            ps.setString(2, world.getContextID());
+            ps.executeUpdate();
+            this.connection.close();
+        } catch (SQLException e) {
+            //System.out.print(e);
+        }
+*/
+
     }
 
     @Override
     public void removeBannedUser(User user, SpatialContext world) {
-        // TODO
-        /*
-        PreparedStatement ps = this.connection.prepareStatement("DELETE FROM BAN WHERE USER_ID = "
-                + user.getUserID() + " AND WORLD_ID = " + world.getContextID();
-        ps.executeUpdate();
-        this.connection.close();
-         */
+/*
+        try {
+            PreparedStatement ps = this.connection.prepareStatement("DELETE FROM BAN WHERE USER_ID = "
+                    + user.getUserID() + " AND WORLD_ID = " + world.getContextID();
+            ps.executeUpdate();
+            this.connection.close();
+        } catch (SQLException e) {
+            //System.out.print(e);
+        }
+*/
+
     }
 
     @Override
