b306eafe8cd9725e074095434ce69df6e9af90f5
raoul <ukuvc@student.kit.edu>
2021-07-28 21:30:41 +0200

    Server Model. Fehler korrigiert.

--- Server/src/main/java/model/context/spatial/ISpatialContext.java
+++ Server/src/main/java/model/context/spatial/ISpatialContext.java
@@ -3,6 +3,8 @@ package model.context.spatial;
 import model.context.ContextID;
 import model.context.IContext;
 
+import java.util.Map;
+
 public interface ISpatialContext extends IContext {
     public Map<ContextID, SpatialContext> getPrivateRooms();
     public SpatialMap getMap();
