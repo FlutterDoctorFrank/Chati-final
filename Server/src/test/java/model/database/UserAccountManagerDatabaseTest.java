c36918f0aa0385dcb7a2bfb3a0c9142d98860112
yujie <uxwzr@student.kit.edu>
2021-07-31 12:49:17 +0200

    Datenbank Derby createAccountTest(erfolgreiche Situation)

--- Server/src/test/java/model/database/UserAccountManagerDatabaseTest.java
+++ Server/src/test/java/model/database/UserAccountManagerDatabaseTest.java
@@ -5,6 +5,7 @@ import model.user.User;
 import org.junit.*;
 
 import java.sql.*;
+import java.util.UUID;
 
 public class UserAccountManagerDatabaseTest {
 
@@ -44,30 +45,29 @@ public class UserAccountManagerDatabaseTest {
     @Test
     public void createAccountTest() {
 
+        //benutze database.createAccount, um ein neuer Benutzer zu erstellen und Informationen in Datenbank zu speichern
         User test = this.database.createAccount("name", "111");
 
-
-        /*
+        //suche direkt in Datenbank
+        int row = 0;
+        String realName = null;
+        String realPassword = null;
+        UUID realUserID = null;
         try{
             Connection con = DriverManager.getConnection(dbURL);
             Statement st = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
 
-            //Suche der User in Datenbank
+            //Suche der User in Datenbank und erhalte Name und Password
             ResultSet res = st.executeQuery("SELECT * FROM USER_ACCOUNT WHERE USER_NAME = 'name'");
-            int row = 0;
+
             while (res.next()){
                 row ++;
             }
-
-            Assert.assertEquals(1, row);
             if (row == 1) {
                 res.first();
-                String user_name = res.getString("USER_NAME");
-                String avatar_name = res.getString("AVATAR_NAME");
-                Avatar user_avatar = Avatar.valueOf(avatar_name);
-
-                //Assert.assertEquals("name", user_name);
-
+                realName = res.getString("USER_NAME");
+                realPassword = res.getString("USER_PSW");
+                realUserID = UUID.fromString(res.getString("USER_ID"));
 
             } else {
                 System.out.println("mehr als 1 or not exist");
@@ -77,8 +77,10 @@ public class UserAccountManagerDatabaseTest {
             System.out.println(e);
 
         }
-        //Assert.assertEquals(realUser.getUserId(), test.getUserId());
-        */
+        Assert.assertEquals("name", realName);
+        Assert.assertEquals("111", realPassword);
+        Assert.assertEquals(test.getUserId(), realUserID);
+
 
     }
 
