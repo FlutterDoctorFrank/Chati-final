8119c1daa2c62c66b177e86ae3b70a3d5ce4183a
Sven Tretter <ufzpf@student.kit.edu>
2021-08-09 19:06:49 +0200

    Vielleicht ein Fix für die View

--- Client/src/main/java/view/UIComponents/UIComponentTable.java
+++ Client/src/main/java/view/UIComponents/UIComponentTable.java
@@ -1,21 +1,30 @@
 package view.UIComponents;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.scenes.scene2d.ui.*;
-import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 
 public abstract class UIComponentTable extends Table {
     protected Skin skin;
     protected Hud hud;
     protected Label infoLabel;
 
+    private static Skin staticSkin;
+
+    public static Skin getStaticSkin() {
+        if (staticSkin == null) {
+            return staticSkin = new Skin(Gdx.files.internal("shadeui/uiskin.json"));
+        }
+
+        return staticSkin;
+    }
+
     public UIComponentTable(Hud hud) {
         setWidth(hud.getWidth());
         setHeight(hud.getHeight());
         this.hud = hud;
-        skin =  new Skin(Gdx.files.internal("shadeui/uiskin.json"));
+        skin =  getStaticSkin();
         setFillParent(true);
         defaults().space(5);
     }
