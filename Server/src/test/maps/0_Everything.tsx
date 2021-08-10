a2a4383385cf4582c9a2adafed6f719a682ebc7f
Viktor Iliev <uqder@student.kit.edu>
2021-08-10 17:34:56 +0200

    map im server hinzugefügt

---
+++ Server/src/test/maps/0_Everything.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="0_Everything" tilewidth="32" tileheight="32" tilecount="7832" columns="8">
+ <image source="World_Tileset/0_Everything.png" width="256" height="31344"/>
+</tileset>
---
+++ Server/src/test/maps/0_Everything_2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="0_Everything_2" tilewidth="32" tileheight="32" tilecount="2048" columns="8">
+ <image source="World_Tileset/0_Everything_2.png" width="256" height="8192"/>
+</tileset>
