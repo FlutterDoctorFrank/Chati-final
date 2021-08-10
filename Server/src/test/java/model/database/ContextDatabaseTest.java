817cd94e3ad37b63d3f37e602a54826dce4e48e3
yujie <uxwzr@student.kit.edu>
2021-08-10 21:58:26 +0200

    Datenbank add/remove/getWorld/s

--- Server/src/test/java/model/database/ContextDatabaseTest.java
+++ Server/src/test/java/model/database/ContextDatabaseTest.java
@@ -1,15 +1,18 @@
 package model.database;
 
+import model.context.ContextID;
 import model.context.global.GlobalContext;
 import model.context.spatial.SpatialMap;
 import model.context.spatial.World;
 import model.role.Role;
 import model.user.User;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.sql.*;
+import java.util.Map;
 
 public class ContextDatabaseTest {
 
@@ -56,18 +59,48 @@ public class ContextDatabaseTest {
     @Test
     public void addWorldTest() {
 
-
         GlobalContext test_gbc = GlobalContext.getInstance();
         User performer = this.account_database.createAccount("performer", "111");
         this.user_database.addRole(performer, test_gbc, Role.OWNER);
         World test = new World("test_world", SpatialMap.MAP);
-        System.out.println(test.getMap().getName());
+        //System.out.println(test.getMap().getName());
+        /*
         try {
             test_gbc.createWorld(performer.getUserId(), "test_world", SpatialMap.MAP);
         } catch (Exception e) {
             e.printStackTrace();
         }
+        Assert.assertEquals(1, test_gbc.getWorlds().size());
+
+         */
+
         this.context_database.addWorld(test);
+
+        //Suche direkt in Datenbank
+        try {
+            Connection con = DriverManager.getConnection(dbURL);
+            Statement st = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
+            ResultSet res = st.executeQuery("SELECT * FROM WORLDS WHERE WORLD_NAME = '" + test.getContextName() + "'");
+            int count = 0;
+            if (res.next()) {
+                count ++;
+            }
+
+            Assert.assertEquals(1, count);
+            res.first();
+            String real_world_name = res.getString("WORLD_NAME");
+            String real_map_name = res.getString("MAP_NAME");
+
+            Assert.assertEquals("test_world", real_world_name);
+            Assert.assertEquals(SpatialMap.MAP.name(), real_map_name);
+
+            st.close();
+            con.close();
+        } catch (SQLException e) {
+            System.out.print("Fehler in deleteData: " + e);
+        }
+
+
 
 
 
@@ -78,35 +111,98 @@ public class ContextDatabaseTest {
 
     @Test
     public void removeWorldTest() {
+        GlobalContext test_gbc = GlobalContext.getInstance();
+        User performer = this.account_database.createAccount("performer", "111");
+        this.user_database.addRole(performer, test_gbc, Role.OWNER);
+        World test = new World("test_world", SpatialMap.MAP);
         /*
-        //Füge eine Welt direkt in Datenbank
+        //add World
         try {
-            Connection con = DriverManager.getConnection(dbURL);
-            PreparedStatement ps = con.prepareStatement("INSERT INTO WORLDS(WORLD_ID, WORLD_NAME, MAP_NAME) values(?,?,?)");
-            World test = new World("test_world", SpatialMap.PLACEHOLDER);
-            ps.setString(1, "12345");
-            ps.setString(2, "test_world");
-            ps.setString(3, "test_map_name");
-            ps.executeUpdate();
-            con.close();
+            test_gbc.createWorld(performer.getUserId(), "test_world", SpatialMap.MAP);
         } catch (Exception e) {
-            System.out.print(e);
+            e.printStackTrace();
         }
-
-        //löschen mit removeWorld
+        Assert.assertEquals(1, test_gbc.getWorlds().size());
 
          */
+        this.context_database.addWorld(test);
+        //Pruefe ob eine Welt echt hinzugefuegt ist
+        try {
+            Connection con = DriverManager.getConnection(dbURL);
+            Statement st = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
+            ResultSet res = st.executeQuery("SELECT * FROM WORLDS WHERE WORLD_NAME = '" + test.getContextName() + "'");
+            int count = 0;
+            if (res.next()) {
+                count ++;
+            }
+
+            Assert.assertEquals(1, count);
+            res.first();
+            String real_world_name = res.getString("WORLD_NAME");
+            String real_map_name = res.getString("MAP_NAME");
+
+            Assert.assertEquals("test_world", real_world_name);
+            Assert.assertEquals(SpatialMap.MAP.name(), real_map_name);
+
+            st.close();
+            con.close();
+        } catch (SQLException e) {
+            System.out.print("Fehler in deleteData: " + e);
+        }
+
+        //remove
+        this.context_database.removeWorld(test);
+        try {
+            Connection con = DriverManager.getConnection(dbURL);
+            Statement st = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
+            ResultSet res = st.executeQuery("SELECT * FROM WORLDS WHERE WORLD_NAME = '" + test.getContextName() + "'");
+            int count = 0;
+            if (res.next()) {
+                count ++;
+            }
+
+            Assert.assertEquals(0, count);
+            st.close();
+            con.close();
+        } catch (SQLException e) {
+            System.out.print("Fehler in deleteData: " + e);
+        }
+
+
+
 
 
     }
 
     @Test
     public void getWorldTest() {
+        //GlobalContext test_gbc = GlobalContext.getInstance();
+        //User performer = this.account_database.createAccount("performer", "111");
+        //this.user_database.addRole(performer, test_gbc, Role.OWNER);
+        World test = new World("test_world", SpatialMap.MAP);
+        //add
+        this.context_database.addWorld(test);
+
+        //getWorld
+        World real = this.context_database.getWorld(test.getContextId());
+        Assert.assertEquals(test, real);
+
 
     }
 
     @Test
     public void getWorldsTest() {
+        World test1 = new World("test1_world", SpatialMap.MAP);
+        World test2 = new World("test2_world", SpatialMap.MAP);
+        this.context_database.addWorld(test1);
+        this.context_database.addWorld(test2);
+
+        //getWorlds
+        Map<ContextID, World> real_worlds = this.context_database.getWorlds();
+        Assert.assertEquals(2, real_worlds.size());
+        Assert.assertEquals("test1_world", real_worlds.get(test1.getContextId()).getContextName());
+        Assert.assertEquals("test2_world", real_worlds.get(test2.getContextId()).getContextName());
+
 
     }
 
