02bc2a8d1622d9bfe038975a7227514e9e3f4830
raoul <ukuvc@student.kit.edu>
2021-08-11 18:05:28 +0200

    Dateinamen korrigiert.

--- Client/src/main/java/view2/component/hud/HeadUpDisplay.java
+++ Client/src/main/java/view2/component/hud/HeadUpDisplay.java
@@ -5,7 +5,6 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
