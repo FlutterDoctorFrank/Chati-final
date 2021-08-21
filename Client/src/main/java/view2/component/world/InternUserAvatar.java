0c89a4eb6f34065174535d62352a5bcf4843d404
raoul <ukuvc@student.kit.edu>
2021-08-21 19:57:18 +0200

    Test

--- Client/src/main/java/view2/component/world/InternUserAvatar.java
+++ Client/src/main/java/view2/component/world/InternUserAvatar.java
@@ -1,7 +1,6 @@
 package view2.component.world;
 
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.World;
 import controller.network.ServerSender;
