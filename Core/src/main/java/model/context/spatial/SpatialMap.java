6e70ce3286ae2b95d317177068989470bdd52702
Sven Tretter <ufzpf@student.kit.edu>
2021-08-25 14:24:42 +0200

    Fehler in den Karten behoben und Karte des vorherigen Commits auskommentiert (War wohl zu langsam mit dem Fix)

--- Core/src/main/java/model/context/spatial/SpatialMap.java
+++ Core/src/main/java/model/context/spatial/SpatialMap.java
@@ -8,7 +8,7 @@ import org.jetbrains.annotations.NotNull;
  */
 public enum SpatialMap implements Resource {
 
-    DEFAULT_WORLD("Standardwelt", "worlds/default-world"),
+    //DEFAULT_WORLD("Standardwelt", "worlds/default-world"),
     PUBLIC_ROOM_MAP("Public Room", "publicRoomMap/publicRoom"),
     PRIVATE_ROOM_MAP("Private Room", "privateRoomMap/privateRoom");
 
