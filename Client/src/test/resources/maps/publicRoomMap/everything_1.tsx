117ded1fb76686a152671a2585d2183deb8c81f3
Viktor Iliev <uqder@student.kit.edu>
2021-08-25 12:46:48 +0200

    map update. hoffe alles wird endlich richtig angezeigt

--- Client/src/test/resources/maps/publicRoomMap/everything_1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.6.0" name="everything_1" tilewidth="32" tileheight="32" tilecount="7832" columns="8">
- <image source="World_Tileset/everything_1.png" width="256" height="31344"/>
-</tileset>
---
+++ Server/src/main/resources/maps/publicRoomMap/19_Hospital_32x32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="19_Hospital_32x32" tilewidth="32" tileheight="32" tilecount="1760" columns="16">
+ <image source="World_Tileset/19_Hospital_32x32.png" width="512" height="3520"/>
+</tileset>
