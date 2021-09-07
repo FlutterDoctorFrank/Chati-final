e544f6470657a4e159eba95865834e646515e2ca
Sven Tretter <ufzpf@student.kit.edu>
2021-09-08 01:09:59 +0200

    Alte Map aus dem Server entfernt (Backup ist notfalls noch vorhanden)

--- Server/src/main/resources/maps/publicRoomMap/ground_2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.6.0" name="ground_2" tilewidth="32" tileheight="32" tilecount="480" columns="16">
- <image source="World_Tileset/ground_2.png" width="512" height="960"/>
-</tileset>
