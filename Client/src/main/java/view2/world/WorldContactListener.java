99235c16a1b0adcf66d4efbcc91134923bfe9c06
raoul <ukuvc@student.kit.edu>
2021-09-08 19:43:57 +0200

    Geister korrigiert.

--- Client/src/main/java/view2/world/WorldContactListener.java
+++ Client/src/main/java/view2/world/WorldContactListener.java
@@ -26,7 +26,7 @@ public class WorldContactListener implements ContactListener {
                 || fixtureB.getBody().getUserData() == ContactType.INTERN_USER)
                 && (fixtureA.getBody().getUserData() == ContactType.INTERACTION_AREA
                 || fixtureB.getBody().getUserData() == ContactType.INTERACTION_AREA)) {
-            if (Chati.CHATI.getWorldScreen().getCurrentInteractableWindow() != null) {
+            if (Chati.CHATI.getWorldScreen().getCurrentInteractableWindow() == null) {
                 return;
             }
             Chati.CHATI.getWorldScreen().getInternUserAvatar().canInteract(false);
